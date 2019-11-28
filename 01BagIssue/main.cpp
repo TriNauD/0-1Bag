86c6dd687d740e4d72b1b26484076e78cfe1531e
Wesrindo <17801202079@163.com>
2019-11-28 14:01:02 +0800

    Initial Commit

---
+++ 01BagIssue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  01BagIssue
+//
+//  Created by Wesrindo on 2019/11/28.
+//  Copyright © 2019 Wesrindo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
